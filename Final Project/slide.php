220cfb79bbdb2719e275a0aab5c0ef1556096a11
nishan1314 <nishandas555@yahoo.com>
2020-02-28 22:26:14 +0600

    project

---
+++ Final Project/slide.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+<div class="container-fluid">
+    <h3 class="text-left">HOT DEALS</h3>
+
+
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+
+
+
+    <div class="carousel-item active">
+      <div class="card" style="width: 18rem;">
+            <img src="image/p5.png" class="card-img-top" alt="...">
+            
+               
+         </div>
+     </div>
+
+
+
+
+      <div class="carousel-item ">
+      <div class="card" style="width: 18rem;">
+            <img src="image/p5.png" class="card-img-top" alt="...">
+                <div class="card-body">
+               <p class="card-text">Fitbit Ionic Fitness Tracker Smart Watch FB503WTGY</p>
+              <span class="card-text" style="color:#00A1B1;">Tk-5200 <strike style="color:red">Tk-6000</strike></span>
+                </div>
+         </div>
+     </div>
+
+
+
+      <div class="carousel-item ">
+      <div class="card" style="width: 18rem;">
+            <img src="image/p5.png" class="card-img-top" alt="...">
+                <div class="card-body">
+               <p class="card-text">Fitbit Ionic Fitness Tracker Smart Watch FB503WTGY</p>
+              <span class="card-text" style="color:#00A1B1;">Tk-5200 <strike style="color:red">Tk-6000</strike></span>
+                </div>
+         </div>
+     </div>
+ </div>
+
+<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+
+
+
+</div>
+</div> 
+
+
+
+
+
+
+    
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
